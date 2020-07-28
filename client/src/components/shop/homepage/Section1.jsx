17ba56514611c97a1ee86bb72ff7546a0ad53d86
kouujaa <kouujaa@gmail.com>
2020-07-28 05:35:32 +0100

    ad sense1

--- client/src/components/shop/homepage/Section1.jsx
+++ client/src/components/shop/homepage/Section1.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Adsense } from "react-adsense";
 
 const ADSense = () => {
   return (
-    <Adsense
-      client="ca-pub-4007877787778046"
-      slot="7259870550"
-      style={{ width: 500, height: 300 }}
-      format=""
-    />
+    <div>
+      <div className="mt-5 mb-5 container">GOOGLE AD SENSE</div>
+    </div>
   );
 };
 
